9b91f3122d0337c55ccc6dedd0feced18b37b90c
Ninh Đan <nguyenninhdan123456@gmail.com>
2024-03-09 20:47:05 +0700

    Blazor Js success

---
+++ BlazorVillaMastery_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using System.Runtime.CompilerServices;
+
+namespace BlazorVillaMastery_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ BlazorVillaMastery_Server/wwwroot/js/common.js
@@ -0,0 +1,27 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, "Operation Successful", {timeOut: 10000});
+    }
+    if (type == "error") {
+        toastr.error(message, "Operation Failed", {timeOut: 10000});
+    }
+
+}
+
+window.ShowSwal = (type, message) => {
+    if (type == "success") {
+        Swal.fire({
+            title: "Success Notification",
+            message,
+            icon: "success"
+        });
+    }
+    if (type == "error") {
+        Swal.fire({
+            title: "Error Notification",
+            message,
+            icon: "error"
+        });
+    }
+
+}
