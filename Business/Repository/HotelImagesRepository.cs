398c00ebac9004716b262212bda165889bf24f58
Ninh Đan <nguyenninhdan123456@gmail.com>
2024-03-09 21:39:13 +0700

    Add Repository HotelImageRoom

--- Business/Repository/HotelImagesRepository.cs
+++ Business/Repository/HotelImagesRepository.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using AutoMapper;
+using Business.Repository.IRepository;
+using DataAcesss.Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Business.Repository
 {
-    internal class HotelImagesRepository
+    public class HotelImagesRepository : IHotelImagesRepository
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public HotelImagesRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+
+        public async Task<int> CreateHotelRoomImage(HotelRoomImageDTO imageDTO)
+        {
+            var image = _mapper.Map<HotelRoomImageDTO, HotelRoomImage>(imageDTO);
+            await _context.HotelRoomImages.AddAsync(image);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteHotelRoomImageByImageId(int imageId)
+        {
+            var image = await _context.HotelRoomImages.FindAsync(imageId);
+            _context.HotelRoomImages.Remove(image);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteHotelRoomImageByRoomId(int roomId)
+        {
+            var imageList = await _context.HotelRoomImages.Where(x => x.RoomId == roomId).ToListAsync();
+            _context.HotelRoomImages.RemoveRange(imageList);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<HotelRoomImageDTO>> GetHotelRoomImages(int roomId)
+        {
+            return _mapper.Map<IEnumerable<HotelRoomImage>, 
+                IEnumerable<HotelRoomImageDTO>>(await _context.HotelRoomImages.Where(x => x.RoomId == roomId).ToListAsync());
+        }
     }
 }
--- Business/Repository/IRepository/IHotelImagesRepository.cs
+++ Business/Repository/IRepository/IHotelImagesRepository.cs
@@ -9,7 +9,7 @@ namespace Business.Repository.IRepository
 {
     public interface IHotelImagesRepository
     {
-        public Task<int> CreateHotelRoomImage(HotelRoomImageDTO image);
+        public Task<int> CreateHotelRoomImage(HotelRoomImageDTO imageDTO);
         public Task<int> DeleteHotelRoomImageByImageId (int imageId);
         public Task<int> DeleteHotelRoomImageByRoomId(int roomId);
         public Task<IEnumerable<HotelRoomImageDTO>> GetHotelRoomImages(int roomId);
